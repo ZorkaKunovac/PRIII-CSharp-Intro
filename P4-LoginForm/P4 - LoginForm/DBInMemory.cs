3f675df06948a3f0433240123d84faff0713be3d
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-09-29 23:16:16 +0200

    Add files via upload
    Lesson 4 and first part of lesson 6. First steps in making Login form. Registration, Validation, DataGrid

---
+++ P4-LoginForm/P4 - LoginForm/DBInMemory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4___LoginForm
+{
+    class DBInMemory
+    {
+        public static List<Korisnik> RegistrovaniKorisnici = new List<Korisnik>();
+
+        static DBInMemory()
+        {
+            RegistrovaniKorisnici = new List<Korisnik>();
+            DodajPodrazumijevaneKorisnike();
+        }
+
+        private static void DodajPodrazumijevaneKorisnike()
+        {
+            Korisnik k1 = new Korisnik() { Id = 1, Ime = "Denis", Prezime = "Music", Username = "denis", Password = "denis" };
+            Korisnik k2 = new Korisnik() { Id = 2, Ime = "Jasmin", Prezime = "Azemovic", Username = "jasmin", Password = "jasmin" };
+            RegistrovaniKorisnici.Add(k1);
+            RegistrovaniKorisnici.Add(k2);
+        }
+    }
+}
