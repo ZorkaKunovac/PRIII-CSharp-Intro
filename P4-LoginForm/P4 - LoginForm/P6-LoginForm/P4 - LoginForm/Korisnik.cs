c31d6c02464ba34c79d52dad700a9118f89abf41
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-09-30 17:20:46 +0200

    Add files via upload

---
+++ P4-LoginForm/P4 - LoginForm/P6-LoginForm/P4 - LoginForm/Korisnik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4___LoginForm
+{
+   public class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Image Slika { get; set; }
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime} ({Username})";
+        }
+
+    }
+}
