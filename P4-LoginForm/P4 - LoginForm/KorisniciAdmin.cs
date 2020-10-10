a8926b700aa6fcd3fc6007d5c69dbeeae0943504
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2020-10-10 21:43:03 +0200

    Add files via upload
    Test app

---
+++ P4-LoginForm/P4 - LoginForm/KorisniciAdmin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4___LoginForm
+{
+    public partial class KorisniciAdmin : Form
+    {
+        public KorisniciAdmin()
+        {
+            InitializeComponent();
+            dvgKorisnici.AutoGenerateColumns = false;
+        }
+
+        private void KorisniciAdmin_Load(object sender, EventArgs e)
+        {
+            dvgKorisnici.DataSource = DBInMemory.RegistrovaniKorisnici;
+        }
+
+        private void btnDodajKorisnika_Click(object sender, EventArgs e)
+        {
+             Registracija registracija = new Registracija();
+           if(registracija.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+    }
+}
